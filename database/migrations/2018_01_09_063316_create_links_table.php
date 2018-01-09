7042b2c96f2e6b1b1e3337eb7d1a6e63f9f60e92
rahajustone <1992raha1@gmail.com>
2018-01-08 22:40:38 -0800

    #1 Setting up DB and creating links model

---
+++ database/migrations/2018_01_09_063316_create_links_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('links', function (Blueprint $table) {
+            $table->incriments('id');
+            $table->text('url');
+            $table->string('hash');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');  
+    }
+}
--- resources/views/forms.blade.php
+++ resources/views/forms.blade.php
@@ -8,7 +8,10 @@
 
 <body>
     <div id="container">
-            <h2>Uber-Shortener</h2> {{Form::open(array('url'=>'/','method'=>'post'))}} {{Form::text('link',Input::old('link'),array('placeholder'=>'Insert your URL here and press enter!'))}} {{Form::close()}}
+        <h2>Uber-Shortener</h2>
+        {{Form::open(array('url'=>'/','method'=>'post'))}}
+        {{Form::text('link',Input::old('link'),array('placeholder'=>'Insert your URL here and press enter!'))}}
+        {{Form::close()}}
     </div>
 </body>
 
